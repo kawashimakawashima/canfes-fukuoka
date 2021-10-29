33a7cb10b090da7cc3c07b4eb6c3b696b2292f08
キタムラ <takeru.hawk.son@gmail.com>
2021-10-29 16:34:43 +0900

    popup〜

--- js/index.js
+++ js/index.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
   setInterval(function(){
     $($(".circles li")[1]).css("background", colors[Math.floor(Math.random() * colors.length)])
   },14000);
+
+  $('.popupButton').on('click',function(){
+    console.log($('.popup.'+$(this).val()));
+    $('.popup.'+$(this).val()).addClass('active');
+});
+  
+$('.close').on('click',function(){
+    $('.popup.'+$(this).val()).removeClass("active");
+});
 })
