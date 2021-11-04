b7a90bdc75812bf1b9b17d0ed06078841c8a3221
キタムラ <takeru.hawk.son@gmail.com>
2021-11-04 10:40:54 +0900

    なんかよーわからんけど

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
   let noCorrectedNumb = 0;
   for(let i = 0; i < questionNames.length; i++){
     if(sessionStorage.getItem(questionNames[i]) == 'false'){
+      console.log('あ');
       noCorrectedNumb++;
     }
   }
